09735532cd593218ac64939a9bc1067ee0faf2dc
CCastro01 <alvescastro00cca13@gmail.com>
2022-01-11 00:37:53 -0300

    Adicionado o card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import Card from "../../components/CardPost";
+
+import fotoDePerfil from "../../assets/nanaths1.png";
+import post1 from "../../assets/post1.png";
+import post2 from "../../assets/post2.png";
 
 export default function Home() {
-  return( 
-  
-    <View style={{ marginTop: 60, marginLeft: 20}}>
-    <Text>Tela de Home</Text>
-  </View>
-  )
+  return (
+    <ScrollView>
+      <View style={{ marginTop: 60 }}>
+        <Card perfil={fotoDePerfil} foto={post1}/>
+        <Card perfil={fotoDePerfil} foto={post1}/>
+        <Card perfil={fotoDePerfil} foto={post1}/>
+        <Card perfil={fotoDePerfil} foto={post1}/>
+        <Card perfil={fotoDePerfil} foto={post1}/>
+
+      </View>
+    </ScrollView>
+  );
 }
