1c42715f5356c83f725d3be95c5e3d682b131fbe
Dya <51456992+bomdya@users.noreply.github.com>
2022-01-17 22:32:33 -0300

    Update index.jsx

--- src/pages/Trending/index.jsx
+++ src/pages/Trending/index.jsx
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+import tags from '../../assets/tagstrend.png'
+import feedTrend from '../../assets/feed-trend.png'
+import styles from "./style";
 
 export default function Trending() {
   return (
-    <View style={{ marginTop: 60, marginLeft: 20}}>
-      <Text> tela de Trending</Text>
-    </View>
-  );
+    
+    <ScrollView style={{ marginTop: 80}}>
+           <View style={{justifyContent: 'flex-start', marginLeft: 15, flexDirection:'row', alignItems: 'flex-start'}}>
+
+              <TouchableOpacity>
+                  <Text style={styles.cursosDestaque}> What's trending </Text>
+              </TouchableOpacity>
+
+           </View>
+
+              <Text style={{marginTop:-10 }}>
+                ____________________________________________________________
+              </Text>
+
+      
+          <ScrollView horizontal={true}>
+            <Image source={tags} style={styles.tagsTrend}/>
+          </ScrollView>
+
+          <View style={{alignItems: 'center'}}>
+            <Image source={feedTrend} style={styles.feedTrend}/>
+          </View>
+    </ScrollView>
+  );  
 }
