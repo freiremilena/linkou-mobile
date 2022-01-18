15a191b15eb5fe73bb757905753d806d03a39406
Dya <51456992+bomdya@users.noreply.github.com>
2022-01-17 22:32:47 -0300

    Update style.jsx

---
+++ src/pages/Trending/style.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create(
+  {
+   
+    cursosDestaque:{
+      fontSize: 20,
+      fontWeight: '700',
+      color: '#555555'
+
+    },
+    tagsTrend:{
+        width: 400,
+        height: 35,
+        marginLeft: 25,
+        marginRight: 25,
+        marginTop: 15
+    },
+    feedTrend:{
+        width: 380,
+        height: 610,
+        marginBottom: 60,
+        marginTop: 40
+    }
+
+}
+)
