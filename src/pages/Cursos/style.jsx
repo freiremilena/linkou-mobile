239ee53435e32567c42775a04004ab858842917a
Dya <51456992+bomdya@users.noreply.github.com>
2022-01-17 22:31:12 -0300

    Update style.jsx

---
+++ src/pages/Cursos/style.jsx
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create(
+  {
+   
+    cursosDestaque:{
+      fontSize: 20,
+      fontWeight: '700',
+      color: '#555555'
+
+    },
+    meusCursos:{
+      fontSize: 16,
+      color: '#6B6A6A',
+      marginLeft: 35,
+      marginTop: 4
+    },
+    cursosTemas:{
+      fontSize: 20,
+      fontWeight: '700',
+      color: '#555555',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+      marginTop: 40,
+      marginBottom: -10
+
+    },
+    imgDestaque:{
+      marginTop: 30,
+      width: 660,
+      height: 190,
+      margin: 15
+
+    },
+    imgTemas:{
+      marginTop: 30,
+      width: 350,
+      height: 258
+
+    },
+    
+    cursosTemasContainer:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'center'
+
+    },
+    
+    imgBtn:{
+      width: 230,
+      height: 47,
+      margin: 30
+    },
+    
+    cursosCert:{
+      fontSize: 20,
+      fontWeight: '700',
+      color: '#555555',
+      marginLeft: 10,
+      marginTop: 40,
+      marginBottom: 15
+
+    },
+    cardCursos:{
+      width: 350,
+      height: 65,
+      elevation: 3, 
+      shadowColor:'#4f4f4f',
+      backgroundColor: '#fff',
+      margin: 5,
+      padding: 10,
+      borderRadius:5
+    
+    },
+    textCard:{
+      fontSize: 16,
+      fontWeight: 'bold'
+    }
+  }
+)
